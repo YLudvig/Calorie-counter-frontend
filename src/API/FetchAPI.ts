540c2c38565108724b138a6a10a2c5bf28af1fa8
YLudvig <yslu24ne@student.ju.se>
2026-06-03 11:04:49 +0200

    Small prod fixes

--- src/API/FetchAPI.ts
+++ src/API/FetchAPI.ts
@@ -1,35 +1,38 @@
 import { logout } from "../Utilities/AuthUtil.ts";
 
-//Ändra vid deploy 
-const BASE_URL = "http://localhost:8080/api";
+//Ändra vid deploy
+const BASE_URL = import.meta.env.VITE_API_URL + "/api";
 
 function getHeaders() {
-    const headers: Record<string, string> = {
-        "Content-Type": "application/json",
-    };
-    const token = localStorage.getItem("token");
-    if (token) {
-        headers["Authorization"] = `Bearer ${token}`;
-    }
-    return headers;
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+  const token = localStorage.getItem("token");
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+  return headers;
 }
 
-export function fetchApi(endpoint: string, options?: RequestInit): Promise<Response> {
-    const url = BASE_URL + endpoint;
-    const fetchOptions: RequestInit = {
-        ...options,
-        headers: {
-            ...getHeaders(),
-            ...(options && options.headers ? options.headers : {}),
-        },
-    };
+export function fetchApi(
+  endpoint: string,
+  options?: RequestInit
+): Promise<Response> {
+  const url = BASE_URL + endpoint;
+  const fetchOptions: RequestInit = {
+    ...options,
+    headers: {
+      ...getHeaders(),
+      ...(options && options.headers ? options.headers : {}),
+    },
+  };
 
-    return fetch(url, fetchOptions).then(function (response) {
-        if (response.status === 401 || response.status === 403) {
-            logout();
-            window.location.replace("/");
-            throw new Error("Unauthorized or forbidden");
-        }
-        return response;
-    });
+  return fetch(url, fetchOptions).then(function (response) {
+    if (response.status === 401 || response.status === 403) {
+      logout();
+      window.location.replace("/");
+      throw new Error("Unauthorized or forbidden");
+    }
+    return response;
+  });
 }
